3eea0d52161d086819157b293c7a26f1aade3456
ForkEyeee <shawncarter123456@gmail.com>
2024-01-28 02:53:13 -0500

    added SignalR boilerplate

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -11,9 +11,8 @@ connection.on("ReceiveMessage", function (user, message) {
     // We can assign user-supplied strings to an element's textContent because it
     // is not interpreted as markup. If you're assigning in any other way, you 
     // should be aware of possible script injection concerns.
-    li.textContent = `${user} says ${message}`;
+    li.textContent = `${user} says ${message}`
 });
-
 connection.start().then(function () {
     document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
