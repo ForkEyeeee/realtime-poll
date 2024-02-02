596c547bee74dfd65270a85f7a37ad5d63ac59b6
ForkEyeee <shawncarter123456@gmail.com>
2024-02-02 02:13:49 -0500

    moved gitignore

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace realTimePolls.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
